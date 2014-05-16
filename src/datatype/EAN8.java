a80ef373cd140680313a58d2da0cd35f5da78b8a
Christian Gläser <glaeser@7says.de>
2014-05-16 09:49:24 +0200

    Added EAN8, essentially a duplicate to EAN13 except for length of value due to request by gearme

---
+++ src/datatype/EAN8.java
@@ -0,0 +1,79 @@
+package datatype;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import foodServer.exceptions.NumberInvalidFormatException;
+
+/**
+ * An implementation of the GS1 specification for the EAN with 13 digits
+ * @author Christian Gläser
+ *
+ */
+public class EAN8 implements IEAN{
+  
+  long ean13;
+  
+  /**
+   * Standard constructor for the EAN
+   */
+  public EAN8() {
+    
+  }
+  
+  public EAN8(long ean) throws NumberInvalidFormatException {
+    setEan(ean);
+  }
+  
+  /**
+   * Returns true if 
+   * @param value The value to check
+   * @return true if number to check is an GS1 EAN13, else false
+   * source: Hibernate Validator v1.4
+   */
+  public boolean isValid(long value) {
+       String stringValue =  new Long(value).toString();
+			if (stringValue == null) return false;
+			if (stringValue.length()!=8) return false;
+			char[] chars = stringValue.toCharArray();
+
+			List<Integer> ints = new ArrayList<Integer>();
+			for (char c : chars) {
+				ints.add( c - '0' );
+			}
+			int length = ints.size();
+			int sum = 0;
+			boolean even = false;
+			for ( int index = length - 1 ; index >= 0 ; index-- ) {
+				int digit = ints.get(index);
+				if  (even) {
+					digit *= 3;
+				}
+				sum+= digit;
+				even = !even;
+			}
+			return  sum % 10 == 0;
+	}
+  
+  /**
+   * @see datatype.IEAN#setEan(long)
+   * @param ean This value is being checked
+   * Sets/Updates the EAN if EAN is valid
+   */
+  public void setEan(long ean) throws NumberInvalidFormatException{
+    if(isValid(ean)){
+      ean13 = ean;
+    }
+    else throw new NumberInvalidFormatException("This is not a valid EAN!");
+  }
+
+/**
+ * @see datatype.IEAN#getEAN()
+ * Returns the value of the EAN as long
+ */
+public long getEAN() {
+	return ean13;
+}
+  
+  
+}
